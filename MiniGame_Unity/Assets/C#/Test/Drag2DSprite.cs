47b641212945404a635f964d6c29b31cf3f4b273
mzy734 <489387032@qq.com>
2021-09-08 21:06:36 +0800

    9.8 scene1
    实现拖拽的交互逻辑

---
+++ MiniGame_Unity/Assets/C#/Test/Drag2DSprite.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Drag2DSprite : MonoBehaviour
+{
+     #region VERSION 1
+    //[SerializeField] private bool isSelected;//MARKER Default Value is False
+
+    //private void OnMouseOver()
+    //{
+    //    if (Input.GetMouseButtonDown(0))
+    //        isSelected = true;
+    //    if (Input.GetMouseButtonUp(0))
+    //        isSelected = false;
+    //}
+
+    //private void Update()
+    //{
+    //    //Debug.Log(Input.mousePosition.x + "-" +  Input.mousePosition.y);//MARKER Watch first
+
+    //    if (isSelected)
+    //    {
+    //        Vector2 cursorWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    //        transform.position = new Vector2(cursorWorldPos.x, cursorWorldPos.y);
+    //    }
+    //}
+    #endregion
+
+    #region VERSION 2
+    [SerializeField] private bool isSelected;
+
+    private void OnMouseDrag()
+    {
+               Vector2 cursorWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+               transform.position = new Vector2(cursorWorldPos.x, cursorWorldPos.y);
+    }
+    #endregion
+
+    private void OnMouseEnter()
+    {
+        //变形操作 体现交互效果
+        //transform.localScale += Vector3.one * 0.1f;
+    }
+
+    private void OnMouseExit()
+    {
+        //变形操作 体现交互效果
+        //transform.localScale -= Vector3.one * 0.1f;
+    }
+}
---
+++ MiniGame_Unity/Assets/C#/Test/FirstManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FirstManager : MonoBehaviour
+{
+
+    public static int change = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (change == 2)
+        {
+            SceneManager.LoadScene("1");
+        }
+    }
+}
