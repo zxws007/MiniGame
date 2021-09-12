9d74a6a93c46f00278c4427a53b18c99268f2c3a
winsonyslu <winsonyslu@tencent.com>
2021-09-12 23:18:59 +0800

    feat(daoyao): 添加捣药scene

---
+++ MiniGame_Unity/Assets/C#/Test/daoyao/DaoYaoHerb.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DaoYaoHerb : MonoBehaviour {
+
+    [SerializeField] public Transform correctTrans;
+    private Vector3 OriginalPos;
+    private bool IsFinished = false;
+    private Renderer Renderer;
+	// Use this for initialization
+	void Start () {
+        OriginalPos = transform.position;
+        Renderer = GetComponent<Renderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    private void OnMouseDrag()
+    {
+        Debug.Log("OnMouseDrag");
+        if (!IsFinished)
+        {
+            transform.position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                             Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        }
+    }
+
+    // Update is called once per frame
+    private void OnMouseUp()
+    {
+        if (Mathf.Abs(transform.position.x - correctTrans.position.x) <= 0.5f &&
+            Mathf.Abs(transform.position.y - correctTrans.position.y) <= 0.5f)
+        {
+            //消失
+            IsFinished = true;
+            Hide();
+            MoveBack();
+        }
+        else
+        {
+            MoveBack();
+        }
+    }
+
+    public void MoveBack()
+    {
+        transform.position = OriginalPos;
+    }
+
+    public void Hide()
+    {
+        Renderer.enabled = false;
+    }
+}
---
+++ MiniGame_Unity/Assets/C#/Test/daoyao/YaoChu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YaoChu : MonoBehaviour {
+    [SerializeField] public int TimesToOver; // 拉手拉完的次数
+    [SerializeField] public float distanceToChange; // 变换状态的滑动距离
+    [SerializeField] public Sprite[] Sprites; // 精灵图
+    private SpriteRenderer SpriteRenderer; // renderer对象
+    private Vector3 mouseDownPos; // 鼠标按下的位置，用于判断距离
+    private bool IsUp = true;
+    private bool IsFinished = false;
+    private int DownTimes = 0; // 捣的次数
+
+    public void RegisterCallbacks()
+    {
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        RegisterCallbacks();
+        SpriteRenderer = GetComponent<SpriteRenderer>();
+        SpriteRenderer.sprite = Sprites[0];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    
+    // 按下时记下当前位置
+    private void OnMouseDown()
+    {
+        mouseDownPos = Input.mousePosition;
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!IsFinished) {
+            // up 状态判断下的距离
+            var currMousePos = Input.mousePosition;
+            Debug.Log(currMousePos);
+            if (IsUp)
+            {
+                var distance = mouseDownPos.y - currMousePos.y;
+                Debug.Log(distance);
+                // 大于距离，改变状态
+                if (distance > distanceToChange)
+                {
+                    SetDown();
+                }
+            }
+            else
+            {
+                var distance = currMousePos.y - mouseDownPos.y;
+                Debug.Log(distance);
+                // 大于距离，改变状态
+                if (distance > distanceToChange)
+                {
+                    SetUp();
+                }
+            }
+        }
+    }
+
+    // Update is called once per frame
+    private void OnMouseUp()
+    {
+    }
+
+    // 设置为up状态
+    private void SetUp()
+    {
+        IsUp = true;
+        SpriteRenderer.sprite = Sprites[0];
+    }
+
+    // 设置为down状态
+    private void SetDown()
+    {
+        IsUp = false;
+        SpriteRenderer.sprite = Sprites[1];
+        DownTimes++;
+        if (DownTimes == TimesToOver)
+        {
+            IsFinished = true;
+        }
+    }
+}
