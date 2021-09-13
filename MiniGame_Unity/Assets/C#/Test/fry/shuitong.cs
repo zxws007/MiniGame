37e48f2833916637917270edcc2156166d74bcf4
mzy734 <489387032@qq.com>
2021-09-13 17:51:23 +0800

    9.13 调整表现效果

---
+++ MiniGame_Unity/Assets/C#/Test/fry/shuitong.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shuitong : MonoBehaviour {
+
+    // Use this for initialization
+    private Vector2 startPos;
+    [SerializeField] private Transform correctTrans;
+    [SerializeField] private bool isFinished;
+
+    public GameObject show1;
+    public GameObject hide1;
+
+    // Use this for initialization
+    private void Start()
+    {
+        startPos = transform.position;
+
+    }
+
+    private void OnMouseDrag()
+    {
+        if (!isFinished)
+        {
+            transform.position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x,
+                                             Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        }
+    }
+
+    // Update is called once per frame
+    private void OnMouseUp()
+    {
+        if (Mathf.Abs(transform.position.x - correctTrans.position.x) <= 0.5f &&
+            Mathf.Abs(transform.position.y - correctTrans.position.y) <= 0.5f)
+        {
+            transform.position = new Vector2(correctTrans.position.x, correctTrans.position.y);
+            isFinished = true;
+            show1.SetActive(true);
+            hide1.SetActive(false);
+
+            
+        }
+        else
+        {
+            transform.position = new Vector2(startPos.x, startPos.y);
+        }
+    }
+}
--- MiniGame_Unity/Assets/C#/Test/make3/Make3Manager.cs
+++ MiniGame_Unity/Assets/C#/Test/make3/Make3Manager.cs
@@ -1,37 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading;
 
 public class Make3Manager : MonoBehaviour {
 
     public static int change = 0;
     public GameObject show1;
     public GameObject show11;
+    public GameObject show12;
     public GameObject hide1;
+    public GameObject hide12;
 
     public GameObject show2;
     public GameObject show22;
+    public GameObject show23;
     public GameObject hide2;
     public GameObject hide22;
+    public GameObject hide23;
 
     public GameObject show3;
     public GameObject show32;
+    public GameObject show33;
     public GameObject hide3;
     public GameObject hide32;
+    public GameObject hide33;
 
     public GameObject show4;
     public GameObject show42;
+    public GameObject show43;
     public GameObject hide4;
     public GameObject hide42;
+    public GameObject hide43;
 
-    public Animator animator;
+    public Animator animator1;
+    public Animator animator2;
+    public Animator animator3;
+    public Animator animator4;
 
 
     public GameObject button;
     // Use this for initialization
     void Start()
     {
-        animator.speed = 0;
+        animator1.speed = 0;
+        animator2.speed = 0;
+        animator3.speed = 0;
+        animator4.speed = 0;
+
     }
 
     // Update is called once per frame
@@ -39,15 +55,18 @@ public class Make3Manager : MonoBehaviour {
     {
         if (change == 1)
         {
-            animator.speed = 1;//播放动画
+            animator1.speed = 1;
             show1.SetActive(true);//切割后的药材
             show11.SetActive(true);//药材片
+            
             hide1.SetActive(false);//未切割的药材
+            
         }
-
         if (change == 2)
         {
-            animator.Play("make3");
+            show12.SetActive(true);
+            hide12.SetActive(false);
+            animator2.speed = 1;
             show2.SetActive(true);
             show22.SetActive(true);
             hide2.SetActive(false);//旧药材片
@@ -57,7 +76,9 @@ public class Make3Manager : MonoBehaviour {
 
         if (change == 3)
         {
-            animator.speed = 1;
+            show23.SetActive(true);
+            hide23.SetActive(false);
+            animator3.speed = 1;
             show3.SetActive(true);
             show32.SetActive(true);
             hide3.SetActive(false);//旧药材片
@@ -66,7 +87,9 @@ public class Make3Manager : MonoBehaviour {
         }
         if (change == 4)
         {
-            animator.speed = 1;
+            show33.SetActive(true);
+            hide33.SetActive(false);
+            animator4.speed = 1;
             show4.SetActive(true);
             show42.SetActive(true);
             hide4.SetActive(false);//旧药材片
