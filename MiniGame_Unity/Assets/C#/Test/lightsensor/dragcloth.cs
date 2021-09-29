a47ea96bf4e58c20192ba4d8cd7eb0bc127a211f
zxws007 <45426317+zxws007@users.noreply.github.com>
2021-09-29 20:16:48 +0800

    /
    8

--- MiniGame_Unity/Assets/C#/Test/lightsensor/dragcloth.cs
+++ MiniGame_Unity/Assets/C#/Test/lightsensor/dragcloth.cs
@@ -2,25 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class dragcloth : MonoBehaviour {
+public class dragcloth : MonoBehaviour
+{
     public GameObject ArrowCloth;
     public GameObject PutCloth;
     public GameObject ArrowBottle;
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
     }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
     private void OnMouseDrag()
     {
-        if (GameObject.Find("GameManager").GetComponent<RunManager>().getHerbready()&& !GameObject.Find("GameManager").GetComponent<RunManager>().getClothready())
+        if (GameObject.Find("GameManager").GetComponent<RunManager>().getHerbready() && !GameObject.Find("GameManager").GetComponent<RunManager>().getClothready())
         {
             Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            
-            if (cursorPos.x > GameObject.Find("ClothEnd").transform.position.x && cursorPos.y <GameObject.Find("ClothUpperBound").transform.position.y && cursorPos.y > GameObject.Find("ClothLowerBound").transform.position.y)
+
+            if (cursorPos.x > GameObject.Find("ClothEnd").transform.position.x && cursorPos.y < GameObject.Find("ClothUpperBound").transform.position.y && cursorPos.y > GameObject.Find("ClothLowerBound").transform.position.y)
             {
                 transform.position = new Vector2(cursorPos.x, cursorPos.y);
             }
@@ -51,12 +54,12 @@ public class dragcloth : MonoBehaviour {
     }
     void OnMouseUp()
     {
-        if (GameObject.Find("GameManager").GetComponent<RunManager>().getClothready() == false&&transform.position.x < 7 && transform.position.y >= -1.5 && transform.position.y <= 1.5)
+        if (GameObject.Find("GameManager").GetComponent<RunManager>().getClothready() == false && transform.position.x < 7 && transform.position.y >= -1.5 && transform.position.y <= 1.5)
         {
             GameObject.Find("GameManager").GetComponent<RunManager>().setClothready(true);
             ArrowBottle.SetActive(true);
         }
-        
+
     }
     void OnCollisionExit2D(Collision2D col)
     {
