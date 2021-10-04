0788413e33ff058b3c3ff54c887faff596179df4
zxws007 <45426317+zxws007@users.noreply.github.com>
2021-10-04 21:08:26 +0800

    333333
    33333333

--- MiniGame_Unity/Assets/KapaiManager.cs
+++ MiniGame_Unity/Assets/KapaiManager.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class KapaiManager : MonoBehaviour
 {
-    public List<string> successlist = new List<string>();
+    bool b1 = false;
+    bool b2 = false;
+    bool b3 = false;
+    bool b4 = false;
+    bool b5 = false;
+    bool b6 = false;
+    bool b7 = false;
+    bool b8 = false;
     public GameObject chaihu_1;
     public GameObject huagnqin_2;
     public GameObject dangshen_3;
@@ -63,25 +70,22 @@ public class KapaiManager : MonoBehaviour
         if (gameObject.scene.name == "05")
         {
             liucheng = 0;
-            successlist.Clear();
-            successlist.Add("1");
-            successlist.Add("2");
-            successlist.Add("3");
+            b1 = true;
+            b2 = true;
+            b3 = true;
         }
         if (gameObject.scene.name == "13")
         {
             liucheng = 1;
-            successlist.Clear();
-            successlist.Add("4");
-            successlist.Add("5");
-            successlist.Add("6");
+            b4 = true;
+            b5 = true;
+            b6 = true;
         }
         if (gameObject.scene.name == "21")
         {
             liucheng = 2;
-            successlist.Clear();
-            successlist.Add("7");
-            successlist.Add("8");
+            b7 = true;
+            b8 = true;
         }
         beginTrans1 = chaihu_1.transform.position;
         beginTrans2 = huagnqin_2.transform.position;
@@ -133,7 +137,7 @@ public class KapaiManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (successlist.Count==0)
+        if (!b1 && !b2 && !b3 && !b4 && !b5 && !b6 && !b7 && !b8)
         {
             next.SetActive(true);
             Time.timeScale = 0;
@@ -142,29 +146,17 @@ public class KapaiManager : MonoBehaviour
         if (Mathf.Abs(chaihu_1.transform.position.x - anban.transform.position.x) <= 500 &&
 Mathf.Abs(chaihu_1.transform.position.y - anban.transform.position.y) <= 100)
         {
-            if (successlist.Contains(chaihu_1.name))
+            if (b1 && !do1)
             {
                 as_dui.Play();
                 count++;
+                dui.SetActive(true);
+                cuo.SetActive(false);
                 AddPos(chaihu_11, count);
-                int index = -1;
-                for (int i = 0; i < successlist.Count; i++)
-                {
-                    if (successlist[i] == chaihu_1.name)
-                    {
-                        index = i;
-                    }
-                }
-                successlist.RemoveAt(index);
-                if (index != -1)
-                {
-                    dui.SetActive(true);
-                    cuo.SetActive(false);
-                    AddPos(chaihu_11, count);
-                    chaihu_11.SetActive(true);
-                    chaihu_1.SetActive(false);
-                }
+                chaihu_11.SetActive(true);
+                chaihu_1.SetActive(false);
                 do1 = true;
+                b1 = false;
             }
             else
             {
@@ -181,29 +173,17 @@ Mathf.Abs(chaihu_1.transform.position.y - anban.transform.position.y) <= 100)
         if (Mathf.Abs(huagnqin_2.transform.position.x - anban.transform.position.x) <= 500 &&
 Mathf.Abs(huagnqin_2.transform.position.y - anban.transform.position.y) <= 100)
         {
-            if (successlist.Contains(huagnqin_2.name))
+            if (b2 && !do2)
             {
                 as_dui.Play();
                 count++;
+                dui.SetActive(true);
+                cuo.SetActive(false);
                 AddPos(huagnqin_22, count);
-                int index = -1;
-                for (int i = 0; i < successlist.Count; i++)
-                {
-                    if (successlist[i] == huagnqin_2.name)
-                    {
-                        index = i;
-                    }
-                }
-                successlist.RemoveAt(index);
-                if (index != -1)
-                {
-                    dui.SetActive(true);
-                    cuo.SetActive(false);
-                    AddPos(huagnqin_22, count);
-                    huagnqin_22.SetActive(true);
-                    huagnqin_2.SetActive(false);
-                }
+                huagnqin_22.SetActive(true);
+                huagnqin_2.SetActive(false);
                 do2 = true;
+                b2 = false;
             }
             else
             {
@@ -219,29 +199,17 @@ Mathf.Abs(huagnqin_2.transform.position.y - anban.transform.position.y) <= 100)
         if (Mathf.Abs(dangshen_3.transform.position.x - anban.transform.position.x) <= 500 &&
 Mathf.Abs(dangshen_3.transform.position.y - anban.transform.position.y) <= 100)
         {
-            if (successlist.Contains(dangshen_3.name))
+            if (b3 && !do3)
             {
                 as_dui.Play();
                 count++;
+                dui.SetActive(true);
+                cuo.SetActive(false);
                 AddPos(dangshen_33, count);
-                int index = -1;
-                for (int i = 0; i < successlist.Count; i++)
-                {
-                    if (successlist[i] == dangshen_3.name)
-                    {
-                        index = i;
-                    }
-                }
-                successlist.RemoveAt(index);
-                if (index != -1)
-                {
-                    dui.SetActive(true);
-                    cuo.SetActive(false);
-                    AddPos(dangshen_33, count);
-                    dangshen_33.SetActive(true);
-                    dangshen_3.SetActive(false);
-                }
+                dangshen_33.SetActive(true);
+                dangshen_3.SetActive(false);
                 do3 = true;
+                b3 = false;
             }
             else
             {
@@ -257,29 +225,17 @@ Mathf.Abs(dangshen_3.transform.position.y - anban.transform.position.y) <= 100)
         if (Mathf.Abs(renshen_4.transform.position.x - anban.transform.position.x) <= 500 &&
 Mathf.Abs(renshen_4.transform.position.y - anban.transform.position.y) <= 100)
         {
-            if (successlist.Contains(renshen_4.name))
+            if (b4 && !do4)
             {
                 as_dui.Play();
                 count++;
+                dui.SetActive(true);
+                cuo.SetActive(false);
                 AddPos(renshen_44, count);
-                int index = -1;
-                for (int i = 0; i < successlist.Count; i++)
-                {
-                    if (successlist[i] == renshen_4.name)
-                    {
-                        index = i;
-                    }
-                }
-                successlist.RemoveAt(index);
-                if (index != -1)
-                {
-                    dui.SetActive(true);
-                    cuo.SetActive(false);
-                    AddPos(renshen_44, count);
-                    renshen_44.SetActive(true);
-                    renshen_4.SetActive(false);
-                }
+                renshen_44.SetActive(true);
+                renshen_4.SetActive(false);
                 do4 = true;
+                b4 = false;
             }
             else
             {
@@ -295,29 +251,17 @@ Mathf.Abs(renshen_4.transform.position.y - anban.transform.position.y) <= 100)
         if (Mathf.Abs(baishu_5.transform.position.x - anban.transform.position.x) <= 500 &&
 Mathf.Abs(baishu_5.transform.position.y - anban.transform.position.y) <= 100)
         {
-            if (successlist.Contains(baishu_5.name))
+            if (b5 && !do5)
             {
                 as_dui.Play();
                 count++;
+                dui.SetActive(true);
+                cuo.SetActive(false);
                 AddPos(baishu_55, count);
-                int index = -1;
-                for (int i = 0; i < successlist.Count; i++)
-                {
-                    if (successlist[i] == baishu_5.name)
-                    {
-                        index = i;
-                    }
-                }
-                successlist.RemoveAt(index);
-                if (index != -1)
-                {
-                    dui.SetActive(true);
-                    cuo.SetActive(false);
-                    AddPos(baishu_55, count);
-                    baishu_55.SetActive(true);
-                    baishu_5.SetActive(false);
-                }
+                baishu_55.SetActive(true);
+                baishu_5.SetActive(false);
                 do5 = true;
+                b5 = false;
             }
             else
             {
@@ -333,29 +277,17 @@ Mathf.Abs(baishu_5.transform.position.y - anban.transform.position.y) <= 100)
         if (Mathf.Abs(fuling_6.transform.position.x - anban.transform.position.x) <= 500 &&
 Mathf.Abs(fuling_6.transform.position.y - anban.transform.position.y) <= 100)
         {
-            if (successlist.Contains(fuling_6.name))
+            if (b6 && !do6)
             {
                 as_dui.Play();
                 count++;
+                dui.SetActive(true);
+                cuo.SetActive(false);
                 AddPos(fuling_66, count);
-                int index = -1;
-                for (int i = 0; i < successlist.Count; i++)
-                {
-                    if (successlist[i] == fuling_6.name)
-                    {
-                        index = i;
-                    }
-                }
-                successlist.RemoveAt(index);
-                if (index != -1)
-                {
-                    dui.SetActive(true);
-                    cuo.SetActive(false);
-                    AddPos(fuling_66, count);
-                    fuling_66.SetActive(true);
-                    fuling_6.SetActive(false);
-                }
+                fuling_66.SetActive(true);
+                fuling_6.SetActive(false);
                 do6 = true;
+                b6 = false;
             }
             else
             {
@@ -371,29 +303,17 @@ Mathf.Abs(fuling_6.transform.position.y - anban.transform.position.y) <= 100)
         if (Mathf.Abs(danggui_7.transform.position.x - anban.transform.position.x) <= 500 &&
 Mathf.Abs(danggui_7.transform.position.y - anban.transform.position.y) <= 100)
         {
-            if (successlist.Contains(danggui_7.name))
+            if (b7 && !do7)
             {
                 as_dui.Play();
                 count++;
+                dui.SetActive(true);
+                cuo.SetActive(false);
                 AddPos(danggui_77, count);
-                int index = -1;
-                for (int i = 0; i < successlist.Count; i++)
-                {
-                    if (successlist[i] == danggui_7.name)
-                    {
-                        index = i;
-                    }
-                }
-                successlist.RemoveAt(index);
-                if (index != -1)
-                {
-                    dui.SetActive(true);
-                    cuo.SetActive(false);
-                    AddPos(danggui_77, count);
-                    danggui_77.SetActive(true);
-                    danggui_7.SetActive(false);
-                }
+                danggui_77.SetActive(true);
+                danggui_7.SetActive(false);
                 do7 = true;
+                b7 = false;
             }
             else
             {
@@ -409,29 +329,17 @@ Mathf.Abs(danggui_7.transform.position.y - anban.transform.position.y) <= 100)
         if (Mathf.Abs(dihuang_8.transform.position.x - anban.transform.position.x) <= 500 &&
 Mathf.Abs(dihuang_8.transform.position.y - anban.transform.position.y) <= 100)
         {
-            if (successlist.Contains(dihuang_8.name))
+            if (b8 && !do8)
             {
                 as_dui.Play();
                 count++;
+                dui.SetActive(true);
+                cuo.SetActive(false);
                 AddPos(dihuang_88, count);
-                int index = -1;
-                for (int i = 0; i < successlist.Count; i++)
-                {
-                    if (successlist[i] == dihuang_8.name)
-                    {
-                        index = i;
-                    }
-                }
-                successlist.RemoveAt(index);
-                if (index != -1)
-                {
-                    dui.SetActive(true);
-                    cuo.SetActive(false);
-                    AddPos(dihuang_88, count);
-                    dihuang_88.SetActive(true);
-                    dihuang_8.SetActive(false);
-                }
+                dihuang_88.SetActive(true);
+                dihuang_8.SetActive(false);
                 do8 = true;
+                b8 = false;
             }
             else
             {
